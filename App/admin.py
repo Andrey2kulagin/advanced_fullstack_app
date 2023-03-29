ad69459f7ade4ed697db5a1e441035768c087b27
Кулагин Андрей Андреевич <s121917@guu.ru>
2023-03-29 18:00:08 +0300

    a

--- App/admin.py
+++ App/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from .models import Candidate
+from django.utils.html import format_html
 
 
 class CandidateAdmin(admin.ModelAdmin):
+    list_filter = ['Situation']
     list_display = [
         "first_name",
         "last_name",
@@ -11,7 +13,10 @@ class CandidateAdmin(admin.ModelAdmin):
         'age',
         'phone_number',
         'situation',
-        'created_at'
+        'created_at',
+        'status',
+        '_'
+
 
     ]
     search_fields = [
@@ -28,8 +33,16 @@ class CandidateAdmin(admin.ModelAdmin):
             return None
         else:
             return False
+
     _.boolean = True
-    
 
-
+    def status(self, obj):
+        if obj.situation == 'Approved':
+            color = "#28a745"
+        elif obj.situation == 'Pending':
+            color = "#fea95e"
+        else:
+            color = 'red'
+        return format_html('<strong><p style="color = {};">{}</p></strong>'.format(color, obj))
+    status.allow_tags=True
 admin.site.register(Candidate, CandidateAdmin)
